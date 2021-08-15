66876819c3ba12f945dbabbe4eef518f19a6cc5c
Bryan Reed <brian.reed.software@outlook.com>
2021-08-15 16:26:58 -0700

    updated routes

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const dotenv = require("dotenv")
-
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
-
-const helmet = require("helmet")
-const morgan = require("morgan")
-const userRoute = require("./routes/users")
-
-const authRoute = require("./routes/auth")
+  
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -20,20 +19,13 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
 )
 
 //middleware
-app.use(express.json())
-app.use(helmet())
-app.use(morgan("common"))
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
 
-app.use("/api/user" , userRoute)
-app.use("/api/auth" , authRoute)
-
-app.get("/", (req,res)=>
-res.send("welcome to home page")
-)
-
-app.get("/users", (req,res)=>
-res.send("welcome to user page")
-)
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 const PORT = 4000
 
--- models/User.js
+++ models/User.js
@@ -1,50 +1,64 @@
 const mongoose = require("mongoose")
 
 
-const UserSchema = new mongoose.Schema({
-
-    username:{
-        type:String,
+const UserSchema = new mongoose.Schema(
+    {
+      username: {
+        type: String,
         require: true,
-        min:3,
-        max:20,
-        unique:true,
-    },
-    email: {
+        min: 3,
+        max: 20,
+        unique: true,
+      },
+      email: {
         type: String,
         required: true,
-        min:6,
-    },
-    password: {
+        max: 50,
+        unique: true,
+      },
+      password: {
         type: String,
         required: true,
-        min:6,
-    },
-    profilePicture: {
+        min: 6,
+      },
+      profilePicture: {
         type: String,
         default: "",
-        min:6,
-    },
-    coverPicture: {
+      },
+      coverPicture: {
         type: String,
         default: "",
-        min:6,
+      },
+      followers: {
+        type: Array,
+        default: [],
+      },
+      followings: {
+        type: Array,
+        default: [],
+      },
+      isAdmin: {
+        type: Boolean,
+        default: false,
+      },
+      desc: {
+        type: String,
+        max: 50,
+      },
+      city: {
+        type: String,
+        max: 50,
+      },
+      from: {
+        type: String,
+        max: 50,
+      },
+      relationship: {
+        type: Number,
+        enum: [1, 2, 3],
+      },
     },
-    followers:{
-        type:Array,
-        default:[],
-    },
-    followerins:{
-        type:Array,
-        default:[],
-    },
-    isAdmin:{
-        type:Boolean,
-        default:false,
-    }
-
-
-
-},{timestamps:true})
+    { timestamps: true }
+  );
 
 module.exports = mongoose.model("User" , UserSchema)
